a9e0de2c6eba3b311a4b794b670955c1665551c4
aenonod <abegail.nonod13@gmail.com>
2025-06-17 23:17:27 +0800

    feat: add code for constructor and command to mark a task done

--- task.py
+++ task.py
@@ -1,8 +1,20 @@
 # task.py -> to handle the task input and to return it as object
-# import datetime module
 
-# under class Task:
-# def for constructor to initialize new object
-# def to turn mark as done from false to true
+# import datetime module
+from datetime import datetime
+
+class Task:
+# constructor to initialize new object
+    def __init__(self, taskname, deadline=None, category=None, priority="Normal", done=False):
+        self.taskname = taskname
+        self.deadline = deadline
+        self.category = category
+        self.priority = priority
+        self.done = done
+        
+# to turn mark as done from false to true
+    def to_mark_done(self):
+        self.done = True
+        
 # def to return task list
 # def to return task into object
