323df039ca6e5568f0ef8ab3d581a84959616f4f
aenonod <abegail.nonod13@gmail.com>
2025-06-17 22:54:30 +0800

    docs: add pseudocode to task.py

---
+++ task.py
@@ -0,0 +1,8 @@
+# task.py -> to handle the task input and to return it as object
+# import datetime module
+
+# under class Task:
+# def for constructor to initialize new object
+# def to turn mark as done from false to true
+# def to return task list
+# def to return task into object
