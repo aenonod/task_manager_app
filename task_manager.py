917a9ad335479c5e8cb63dfc7b37e8be33d89c63
aenonod <abegail.nonod13@gmail.com>
2025-06-22 18:55:00 +0800

    docs: update pseudocode of task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -1,11 +1,11 @@
 # task_manager.py -> to manage tasks, append and save it to a file, and mark it done
-# import json
-# import task module
+# import json module
+# import task from task.py
 
 # under TaskManager class:
 # def for constructor to initialize empty task list
+# def to load the file
 # def to append given task to the list
 # def to save it to a file
 # def to check the status of the tasks
 # def to mark task as done
-# def to load the file
