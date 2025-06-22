551c1ae055b5c3a7ddae7aa5771a7cfbc80b332c
aenonod <abegail.nonod13@gmail.com>
2025-06-23 00:21:15 +0800

    fix: errors with command and parameter used

--- task_manager.py
+++ task_manager.py
@@ -20,22 +20,22 @@ class TaskManager:
             self.tasklist = []
               
 # def to append given task to the list
-    def add_task_to_list(self):
+    def add_task_to_list(self, task):
         self.tasklist.append(task)
 
 # def to save it to a file
     def save_to_file(self, filename="tasks.json"):
-        with open(filename, "r") as file:
+        with open(filename, "w") as file:
             json.dump([task.to_dict() for task in self.tasklist], file)
   
 # def to check the tasks
     def list_of_task(self):
         for i, task in enumerate(self.tasklist):
             status = "✅" if task.done else "❌"
-            print(f"[{status}] {1+1}. {task.taskname} - Priority: {self.priority}")
+            print(f"[{status}] {i+1}. {task.taskname} - Priority: {task.priority}")
             
 # def to mark task as done
-    def mark_task_as_done(self):
+    def mark_task_as_done(self, index):
         if 0 <= index < len(self.tasklist):
             self.tasklist[index].to_mark_done()
         else:
