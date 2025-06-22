7efb2274e872bdd6218795543356fda8f0d57b8f
aenonod <abegail.nonod13@gmail.com>
2025-06-22 23:21:01 +0800

    docs: pseudocode to app_menu.py

---
+++ app_menu.py
@@ -0,0 +1,8 @@
+# app_menu.py -> contains the main menu of the app
+# import datetime module
+# import task from task.py
+# import task manager from task_manager.py
+
+# under AppMenu class:
+# def to view the home menu (choices)
+# def to run the user choice
