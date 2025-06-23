90255755d38161c90f868c584f902efbfe41ab87
aenonod <abegail.nonod13@gmail.com>
2025-06-23 23:21:38 +0800

    docs: add pseudocode to timed_task.py

---
+++ timed_task.py
@@ -0,0 +1,5 @@
+# timed_task.py -> to inherit task class and to add type and duration into the dict
+
+# under TimedTask class:
+# def to initialize previous objects with additional info
+# def to return data into dict with additional info
