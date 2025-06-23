ea3495eb59c998b30d5c722d29f4fd893a760524
aenonod <abegail.nonod13@gmail.com>
2025-06-24 00:25:20 +0800

    add program code to run taskmate

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py -> to run the whole program
+
+# import appmenu from app_menu.py
+from app_menu import AppMenu
+
+# call out appmenu class to run
+if __name__ == "__main__":
+    app = AppMenu()
+    app.run() 
