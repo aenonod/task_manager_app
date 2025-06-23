03e4f777ea0b8e4b1804efb00557fc4c49430c7d
aenonod <abegail.nonod13@gmail.com>
2025-06-23 23:28:35 +0800

    add code including type and duration as part of the timed task

--- timed_task.py
+++ timed_task.py
@@ -1,5 +1,17 @@
 # timed_task.py -> to inherit task class and to add type and duration into the dict
 
-# under TimedTask class:
+# import task from task.py
+from task import Task
+
+class TimedTask(Task):
 # def to initialize previous objects with additional info
+    def __init__(self, taskname, deadline, category, priority, done=False, duration=0):
+        super().__init__(taskname, deadline, category, priority, done)
+        self.duration = duration
+        
 # def to return data into dict with additional info
+    def to_dict(self):
+        data = super().to_dict()
+        data["type"] = "TimedTask"
+        data["duration"] = self.duration
+        return data
