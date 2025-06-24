64fb97d3ce9328963d962c9c1cbe8d52e164b485
aenonod <abegail.nonod13@gmail.com>
2025-06-24 13:10:48 +0800

    docs: add pseudocode to app_gui.py

---
+++ app_gui.py
@@ -0,0 +1,12 @@
+# app_gui -> to turn text based app into gui
+# import datetime module
+# import task from task.py
+# import taskmanager from task_manager.py
+# import timedtask from timed_task.py
+# import tkinter
+
+# create a window
+# create frame 1 for main menu
+# create frame 2 to add task
+# create frame 3 for task list
+# create frame 4 to meet the dev
