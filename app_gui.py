4434c77768f9e9c105aead719250bcde2ff23c97
aenonod <abegail.nonod13@gmail.com>
2025-06-24 16:22:02 +0800

    feat: add buttons to add task, view list, and meet the dev

--- app_gui.py
+++ app_gui.py
@@ -8,6 +8,8 @@ from task import Task
 from task_manager import TaskManager
 # import timedtask from timed_task.py
 from timed_task import TimedTask
+# import appmenu from app_menu.py
+from app_menu import AppMenu
 # import tkinter
 import tkinter as tk
 
@@ -23,19 +25,51 @@ window.iconphoto(True, icon)
 window.config(background="#A095A7")
 
 # create frame 1 for main menu
+# sub welcome message: "What do you want to do?"
 subwelcome_label = tk.Label(window,
                       text="What do you want to do?",
-                      font=('Segoe UI', 15),   # Courier New Segoe UI
+                      font=('Segoe UI', 15),
                       fg="#000000",
                       bg="#A095A7")
-subwelcome_label.place(relx=0.5, y=150, anchor="center")
-
+subwelcome_label.place(relx=0.5, y=160, anchor="center")
+# welcome message: "Hello, Taskmate!"
 welcome_label = tk.Label(window,
                       text="Hello, Taskmate!",
                       font=('Century Gothic', 40, 'bold', 'underline'),
                       fg="#000000",
                       bg="#A095A7")
-welcome_label.place(relx=0.5, y=100, anchor="center")
+welcome_label.place(relx=0.5, y=110, anchor="center")
+# button 1: add task
+menu = AppMenu()
+add_task_button = tk.Button(window,
+                      text="Add Task",
+                      font=('Arial', 13),
+                      fg="#000000",
+                      bg="#E5DBF3",
+                      padx=45,
+                      pady=10)
+add_task_button.config(command=menu.add_task)
+add_task_button.place(relx=0.5, y=280,anchor="center")
+# button 2: view task list
+view_task_button = tk.Button(window,
+                      text="View List of Tasks",
+                      font=('Arial', 13),
+                      fg="#000000",
+                      bg="#E5DBF3",
+                      padx=15,
+                      pady=10)
+view_task_button.config(command=menu.view_task)
+view_task_button.place(relx=0.5, y=345,anchor="center")
+# button 3: meet the dev
+meet_dev_button = tk.Button(window,
+                      text="Meet the Developer",
+                      font=('Arial', 13),
+                      fg="#000000",
+                      bg="#E5DBF3",
+                      padx=10,
+                      pady=10)
+meet_dev_button.config(command=menu.meet_the_dev)
+meet_dev_button.place(relx=0.5, y=410,anchor="center")
 
 window.mainloop()
 
