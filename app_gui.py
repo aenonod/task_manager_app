764442ced89ec84d56e1dc283204e831d790ee0f
aenonod <abegail.nonod13@gmail.com>
2025-06-24 14:15:11 +0800

    feat: add welcome and subwelcome message on main menu frame

--- app_gui.py
+++ app_gui.py
@@ -17,14 +17,28 @@ task_manager.load_file()
 # create a window
 window = tk.Tk()
 window.title("TASKMATE")
-window.geometry("400x400")
+window.geometry("800x600")
 icon = tk.PhotoImage(file='cool_logo.png')
 window.iconphoto(True, icon)
 window.config(background="#A095A7")
 
+# create frame 1 for main menu
+subwelcome_label = tk.Label(window,
+                      text="What do you want to do?",
+                      font=('Segoe UI', 15),   # Courier New Segoe UI
+                      fg="#000000",
+                      bg="#A095A7")
+subwelcome_label.place(relx=0.5, y=150, anchor="center")
+
+welcome_label = tk.Label(window,
+                      text="Hello, Taskmate!",
+                      font=('Century Gothic', 40, 'bold', 'underline'),
+                      fg="#000000",
+                      bg="#A095A7")
+welcome_label.place(relx=0.5, y=100, anchor="center")
+
 window.mainloop()
 
-# create frame 1 for main menu
 # create frame 2 to add task
 # create frame 3 for task list
 # create frame 4 to meet the dev
