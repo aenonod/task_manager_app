e6d3e6c6ad12e5fafb3e87567d76e7ef534363c0
aenonod <abegail.nonod13@gmail.com>
2025-06-24 13:33:39 +0800

    add code to create a window

--- app_gui.py
+++ app_gui.py
@@ -1,11 +1,29 @@
 # app_gui -> to turn text based app into gui
+
 # import datetime module
+from datetime import datetime
 # import task from task.py
+from task import Task
 # import taskmanager from task_manager.py
+from task_manager import TaskManager
 # import timedtask from timed_task.py
+from timed_task import TimedTask
 # import tkinter
+import tkinter as tk
+
+task_manager = TaskManager()
+task_manager.load_file()
 
 # create a window
+window = tk.Tk()
+window.title("TASKMATE")
+window.geometry("400x400")
+icon = tk.PhotoImage(file='cool_logo.png')
+window.iconphoto(True, icon)
+window.config(background="#A095A7")
+
+window.mainloop()
+
 # create frame 1 for main menu
 # create frame 2 to add task
 # create frame 3 for task list
